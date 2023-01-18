efbc6b6cae829029da0521e3aa198dfab182a82f
Santhosh S <39198124+Santhosh-S14@users.noreply.github.com>
2023-01-17 23:24:57 -0500

    Setting up Babel

--- react-course-projects/indecision-app/src/app.js
+++ react-course-projects/indecision-app/src/app.js
@@ -1,15 +1,9 @@
-'use strict';
-
 // Features of React and React DOM
 // JSX - JavaScript XML
 
 console.log('App.js is running!');
 
-var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
-);
+var template = <h1>Indecision App</h1>;
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
