78b43c087e13518185aa721f9247fa13e436d8f7
Santhosh S <39198124+Santhosh-S14@users.noreply.github.com>
2023-01-17 23:01:15 -0500

    Added first JSX extension & converted to ES5-JS

--- react-course-projects/indecision-app/public/scripts/app.js
+++ react-course-projects/indecision-app/public/scripts/app.js
@@ -2,3 +2,12 @@
 // JSX - JavaScript XML
 
 console.log('App.js is running!');
+
+// var template = <p>This is JSX from app.js!</p>;
+
+var template = React.createElement("h1", {
+    id: ""
+  }, "Something new!!");
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
