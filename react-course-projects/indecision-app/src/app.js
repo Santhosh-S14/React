8eb481a81701c685e446f8bc48c08835536f83d1
Santhosh S <39198124+Santhosh-S14@users.noreply.github.com>
2023-01-18 23:47:11 -0500

    Exploring JSX, expressions and conditional rendering

--- react-course-projects/indecision-app/src/app.js
+++ react-course-projects/indecision-app/src/app.js
@@ -3,7 +3,43 @@
 
 console.log('App.js is running!');
 
-var template = <h1>Indecision App</h1>;
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
+};
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{app.options.length > 0 ? 'Here are your options': 'No options'}</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+var user = {
+    name: 'Santhosh Santhanam',
+    age: 25,
+    location: 'Montreal'
+};
+
+function getLocation(location){
+    if(location){
+        return <p>Location: {location}</p>;
+    }
+}
+var templateTwo = (
+    <div>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
+    </div>
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
+// ReactDOM.render(templateTwo, appRoot);
