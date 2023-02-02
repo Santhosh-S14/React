f76fb36006413dbb4abfb5b4472e2232bda4f128
Santhosh S <39198124+Santhosh-S14@users.noreply.github.com>
2023-02-02 01:02:07 -0500

    Reach components state example

--- react-course-projects/indecision-app/src/playground/counter-example.js
+++ react-course-projects/indecision-app/src/playground/counter-example.js
@@ -1,52 +1,102 @@
-let count = 0;
-const addOne = () => {
-    console.log('addOne');
-    count++;
-    reactCounterApp();
-};
-const minusOne = () => {
-    console.log('minusOne');
-    count--;
-    reactCounterApp();
-};
-const reset = () => {
-    console.log('reset');
-    count = 0;
-    reactCounterApp();
-};
+class Counter extends React.Component {
 
-const appRoot = document.getElementById('app');
+    constructor(props) {
+        super(props);
+        this.handleAddOne = this.handleAddOne.bind(this);
+        this.handleMinusOne = this.handleMinusOne.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+        this.state = {
+            count: 0
+        };
+    }
 
-const reactCounterApp = () => {
-    const templateThree = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={minusOne}>-1</button>
-            <button onClick={reset}>reset</button>
-        </div>
-    );
-    ReactDOM.render(templateThree, appRoot);
-};
+    handleAddOne() {
+        this.setState((prevState) => {
+            return {
+                count: prevState.count + 1
+            };
+        });
+    }
 
-reactCounterApp();
+    handleMinusOne() {
+        this.setState((prevState) => {
+            return {
+                count: prevState.count - 1
+            };
+        });
+    }
 
+    handleReset() {
+        this.setState(() => {
+            return {
+                count: 0
+            };
+        });
+    }
 
-const user = {
-    name: 'Santhosh Santhanam',
-    age: 25,
-    location: 'Montreal'
-};
-
-function getLocation(location) {
-    if (location) {
-        return <p>Location: {location}</p>;
+    render() {
+        return (
+            <div>
+                <h1>Count: {this.state.count}</h1>
+                <button onClick={this.handleAddOne}>+1</button>
+                <button onClick={this.handleMinusOne}>-1</button>
+                <button onClick={this.handleReset}>reset</button>
+            </div>
+        );
     }
 }
-const templateTwo = (
-    <div>
-        <h1>{user.name ? user.name : 'Anonymous'}</h1>
-        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
-        {getLocation(user.location)}
-    </div>
-);
+
+ReactDOM.render(<Counter />, document.getElementById('app'));
+
+// let count = 0;
+// const addOne = () => {
+//     console.log('addOne');
+//     count++;
+//     reactCounterApp();
+// };
+// const minusOne = () => {
+//     console.log('minusOne');
+//     count--;
+//     reactCounterApp();
+// };
+// const reset = () => {
+//     console.log('reset');
+//     count = 0;
+//     reactCounterApp();
+// };
+
+// const appRoot = document.getElementById('app');
+
+// const reactCounterApp = () => {
+//     const templateThree = (
+//         <div>
+//             <h1>Count: {count}</h1>
+//             <button onClick={addOne}>+1</button>
+//             <button onClick={minusOne}>-1</button>
+//             <button onClick={reset}>reset</button>
+//         </div>
+//     );
+//     ReactDOM.render(templateThree, appRoot);
+// };
+
+// reactCounterApp();
+
+
+// const user = {
+//     name: 'Santhosh Santhanam',
+//     age: 25,
+//     location: 'Montreal'
+// };
+
+// function getLocation(location) {
+//     if (location) {
+//         return <p>Location: {location}</p>;
+//     }
+// }
+// const templateTwo = (
+//     <div>
+//         <h1>{user.name ? user.name : 'Anonymous'}</h1>
+//         {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+//         {getLocation(user.location)}
+//     </div>
+// );
