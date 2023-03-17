0f9f861f0c4d5c8786fa4c505aefe476a0e5211e
Santhosh S <39198124+Santhosh-S14@users.noreply.github.com>
2023-03-17 17:41:03 -0400

    One component per file

--- react-course-projects/indecision-app/src/app.js
+++ react-course-projects/indecision-app/src/app.js
@@ -1,24 +1,5 @@
-import subtract, { square, add } from './utils.js';
-import isSenior, { isAdult, canDrink } from './person.js';
-import validator from 'validator';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
 
-console.log('app.js is running!!!');
-
-//named exports
-console.log(square(4));
-console.log(add(100, 23));
-
-//default export
-console.log(subtract(100, 23));
-
-//another example to test
-console.log("Is Adult: ", isAdult(18));
-console.log("Can Drink: ", canDrink(21));
-console.log("Is Senior: ", isSenior(64));
-
-console.log("Verify email: ", validator.isEmail('test@gmail.com'));
-
-const template = <p>THIS IS JSX FROM WEBPACK</p>
-ReactDOM.render(template, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
