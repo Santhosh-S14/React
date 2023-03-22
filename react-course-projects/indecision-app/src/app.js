ae82e5f1fd969a3091488951e4321c88f193ff34
Santhosh S <39198124+Santhosh-S14@users.noreply.github.com>
2023-03-22 18:11:50 -0400

    Architecture for styling is set up | Header styles added

--- react-course-projects/indecision-app/src/app.js
+++ react-course-projects/indecision-app/src/app.js
@@ -4,30 +4,3 @@ import IndecisionApp from './components/IndecisionApp';
 import './styles/styles.scss';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
-
-// class OldSyntax {
-//     constructor() {
-//         this.name = 'Santhosh';
-//         this.getGreeting = this.getGreeting.bind(this);
-//     };
-
-//     getGreeting() {
-//         return `Hi, My name is ${this.name}.`
-//     }
-// }
-
-// class NewSyntax {
-//     name = 'Jen';
-
-//     getGreeting = () => {
-//         return `Hi, My name is ${this.name}.`
-//     }
-// }
-
-// const oldSyntax = new OldSyntax();
-// const getGreeting = oldSyntax.getGreeting;
-// console.log(getGreeting());
-
-// const newSyntax = new NewSyntax();
-// const newGetGreeting = newSyntax.getGreeting;
-// console.log(newGetGreeting());
