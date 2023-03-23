5bfc6e6dd18923083ce9f4ed5ba8e3c606726aa7
Santhosh S <39198124+Santhosh-S14@users.noreply.github.com>
2023-03-23 18:21:41 -0400

    Added all the styles requried for the web app

--- react-course-projects/indecision-app/src/components/Options.js
+++ react-course-projects/indecision-app/src/components/Options.js
@@ -5,17 +5,21 @@ export default class Options extends React.Component {
     render() {
         return (
             <div>
+            <div className="widget-header">
+                <h3 className="widget-header__title">Your Options</h3>
                 <button
                     className="button button--link" 
                     onClick={this.props.handleDeleteOptions}
                 >
                     Remove All
                 </button>
-                {this.props.options.length === 0 && <p>Please enter an option to get started!</p>}
-                {this.props.options.map((option) => 
+            </div>
+                {this.props.options.length === 0 && <p className="widget__message">Please add an option to get started!</p>}
+                {this.props.options.map((option, index) => 
                     <Option 
                         key={option} 
-                        optionText={option} 
+                        optionText={option}
+                        count={index + 1} 
                         handleDeleteOption={this.props.handleDeleteOption}
                     />
                 )}
