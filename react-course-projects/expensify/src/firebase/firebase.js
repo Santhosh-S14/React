77d117087d499a315d0ee07d12219bb420652361
Santhosh S <39198124+Santhosh-S14@users.noreply.github.com>
2023-09-18 15:43:14 -0400

    Firebase functions

--- react-course-projects/expensify/src/firebase/firebase.js
+++ react-course-projects/expensify/src/firebase/firebase.js
@@ -13,8 +13,78 @@ const config = {
 
   firebase.initializeApp(config);
 
-  firebase.database().ref().set(
-    {
-        'name': 'Santhosh Santhanam'
-    }
-  );
+  const database = firebase.database();
+
+  database.ref('expenses').on('value', (snapshot) => {
+      const expenses = [];
+      snapshot.forEach((childSnapshot) => {
+         expenses.push({
+            id: childSnapshot.key,
+            ...childSnapshot.val()
+         });
+      });
+      console.log(expenses);
+  });
+
+//   database.ref('expenses').once('value')
+//    .then((snapshot) => {
+//       const expenses = [];
+
+//       snapshot.forEach((childSnapshot) => {
+//          expenses.push({
+//             id: childSnapshot.key,
+//             ...childSnapshot.val()
+//          });
+//       });
+//       console.log(expenses);
+//    })
+
+//   database.ref('expenses').push({
+//    description: 'Wifi', 
+//    note: 'Sep month',
+//    amount: 5883,
+//    createdAt: 976123498763
+//   });
+
+
+//   const onValueChange = database.ref().on('value', (snapshot) => {
+//    const val = snapshot.val();
+//    console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+//   }, (e) => {
+//    console.log("Error fetching data", e);
+//   });
+
+//   database.ref()
+//    .once('value')
+//    .then((snapshot) => {
+//       const val = snapshot.val();
+//       console.log(val);
+//    })
+//    .catch((e) => {
+//       console.log("Error fetching data ", e);
+//    });
+
+
+//  database.ref().set({
+//     name: 'Santhosh Santhanam',
+//     age: 25,
+//     stressLevel: 6,
+//     job: {
+//       title: 'Software Developer',
+//       company: 'Intact'
+//     },
+//     location: {
+//         city: 'Montreal',
+//         country: 'Canada'
+//     }
+//  }).then(() => {
+//     console.log('Data is saved');
+//  }).catch((e) => {
+//     console.log('This failed', e);
+//  });
+
+//  database.ref().update({
+//    stressLevel: 9,
+//    'job/company': 'Amazon',
+//    'location/city': 'Vancouver'
+//  });
