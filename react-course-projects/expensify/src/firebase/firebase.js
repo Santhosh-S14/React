fc5ae959d42f8f503c210ba0890276f5d38da7cc
Santhosh S <39198124+Santhosh-S14@users.noreply.github.com>
2024-02-17 15:07:40 -0500

    modified/react-course-projects-files|new/namaste-react

--- react-course-projects/expensify/src/firebase/firebase.js
+++ react-course-projects/expensify/src/firebase/firebase.js
@@ -15,16 +15,18 @@ const config = {
 
   const database = firebase.database();
 
-  database.ref('expenses').on('value', (snapshot) => {
-      const expenses = [];
-      snapshot.forEach((childSnapshot) => {
-         expenses.push({
-            id: childSnapshot.key,
-            ...childSnapshot.val()
-         });
-      });
-      console.log(expenses);
-  });
+  export { firebase, database as default };
+
+//   database.ref('expenses').on('value', (snapshot) => {
+//       const expenses = [];
+//       snapshot.forEach((childSnapshot) => {
+//          expenses.push({
+//             id: childSnapshot.key,
+//             ...childSnapshot.val()
+//          });
+//       });
+//       console.log(expenses);
+//   });
 
 //   database.ref('expenses').once('value')
 //    .then((snapshot) => {
