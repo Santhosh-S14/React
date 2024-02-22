4ecb2c81518d06fd24b100567f4e5f819417e7c1
Santhosh S <39198124+Santhosh-S14@users.noreply.github.com>
2024-02-22 00:07:05 -0500

    Created react components

--- Namaste React/App.js
+++ Namaste React/App.js
@@ -1,31 +1,16 @@
 import React from 'react';
 import ReactDOM  from 'react-dom/client';
 
-const heading = React.createElement("h1", {id: "heading"}, "Hello World from React");
-const nestedHeading = React.createElement
-    (
-        "div", 
-        { id: "parent" },
-        [
-            React.createElement
-            (
-                "div", 
-                { id: "child" },
-                [
-                    React.createElement("h1", {}, "I'm a h1 tag"), 
-                    React.createElement("h2", {}, "I'm a h2 tag")
-                ] 
-            ), 
-            React.createElement
-            (
-                "div", 
-                { id: "child2" },
-                [
-                    React.createElement("h1", {}, "I'm a h1 tag"), 
-                    React.createElement("h2", {}, "I'm a h2 tag")
-                ] 
-            )
-        ]
+const Title = () => <h1 id="heading" tabIndex="5">Namaste React using JSX</h1>
+
+const HeadingComponent = () => {
+    return(
+        <div id='container'>
+            <Title />
+            <h1 className='heading'>Namaste React Functional Component</h1>
+        </div>
     );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(nestedHeading);
+root.render(<HeadingComponent />);
