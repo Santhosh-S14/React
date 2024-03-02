d20a21664cd0ac311b896ac990beaf4b68efd91c
Santhosh S <39198124+Santhosh-S14@users.noreply.github.com>
2024-03-02 01:17:12 -0500

    Intro to react hooks

--- Namaste React/src/components/Body.js
+++ Namaste React/src/components/Body.js
@@ -1,17 +1,31 @@
 import RestaurantContainer from "./RestaurantContainer";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
-    return (
-      <div className="body">
-        <div className="search">Search</div>
-        <div className="restaurant-container">
-          {resList.map((restaurant) => (
-            <RestaurantContainer key={ restaurant.info.id } resData={ restaurant } />
-          ))}
-        </div>
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  return (
+    <div className="body">
+      <div className="filter">
+        <button 
+          className="filter-btn" 
+          onClick={ () => {
+            const filteredList = listOfRestaurants.filter( 
+              (res) => res.info.avgRating > 4
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top rated restaurants
+        </button>
       </div>
-    );
+      <div className="restaurant-container">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantContainer key={ restaurant.info.id } resData={ restaurant } />
+        ))}
+      </div>
+    </div>
+  );
   };
 
   export default Body;
