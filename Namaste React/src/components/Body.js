b21fac0045071cf39fcdb0a113011538d67e5030
Santhosh S <39198124+Santhosh-S14@users.noreply.github.com>
2024-03-02 16:35:18 -0500

    Live api call using fetch and added shimmer ui

--- Namaste React/src/components/Body.js
+++ Namaste React/src/components/Body.js
@@ -1,16 +1,35 @@
 import RestaurantContainer from "./RestaurantContainer";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0011774&lng=80.2564957&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    setListOfRestaurants(
+      json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+    );
+  };
+
+  if (listOfRestaurants.length === 0) {
+    return <Shimmer />;
+  }
+
   return (
     <div className="body">
       <div className="filter">
-        <button 
-          className="filter-btn" 
-          onClick={ () => {
-            const filteredList = listOfRestaurants.filter( 
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
               (res) => res.info.avgRating > 4
             );
             setListOfRestaurants(filteredList);
@@ -21,11 +40,11 @@ const Body = () => {
       </div>
       <div className="restaurant-container">
         {listOfRestaurants.map((restaurant) => (
-          <RestaurantContainer key={ restaurant.info.id } resData={ restaurant } />
+          <RestaurantContainer key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
     </div>
   );
-  };
+};
 
-  export default Body;
+export default Body;
