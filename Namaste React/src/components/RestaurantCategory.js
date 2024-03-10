b7f73878732ceea8da9a1f242b8bbbbbd2271e2b
Santhosh S <39198124+Santhosh-S14@users.noreply.github.com>
2024-03-10 07:10:40 -0400

    Added categories to each restaurant and made the UI better

---
+++ Namaste React/src/components/RestaurantCategory.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import Itemlist from "./Itemlist";
+
+const RestaurantCategory = ({ data }) => {
+  const [showItems, setShowItems] = useState();
+  const handleCategories = () => {
+    setShowItems(!showItems);
+  };
+
+  return (
+    <div>
+      <div className="w-6/12 mx-auto my-4 p-4 shadow-lg bg-gray-50 border-b-2 border-gray-200">
+        <div
+          className="flex justify-between cursor-pointer"
+          onClick={handleCategories}
+        >
+          <span className="font-bold text-lg">
+            {data.title}({data.itemCards.length})
+          </span>
+          <span>⬇️</span>
+        </div>
+        <div>{showItems && <Itemlist items={data.itemCards} />}</div>
+      </div>
+    </div>
+  );
+};
+export default RestaurantCategory;
--- Namaste React/src/components/RestaurantMenu.js
+++ Namaste React/src/components/RestaurantMenu.js
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import Shimmer from "./Shimmer";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
+import RestaurantCategory from "./RestaurantCategory";
 
 const RestaurantMenu = () => {
   const { id } = useParams();
@@ -10,7 +11,7 @@ const RestaurantMenu = () => {
     return <Shimmer />;
   }
   const { name, cuisines, costForTwoMessage } =
-    resInfo?.data?.cards[0]?.card?.card?.info;
+    resInfo.data.cards[0].card.card.info;
 
   let itemCards = null;
   if (
@@ -29,21 +30,22 @@ const RestaurantMenu = () => {
         .itemCards;
   }
 
+  const categories =
+    resInfo.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards.filter(
+      (c) =>
+        c.card.card["@type"] ===
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    );
+
   return (
-    <div className="menu">
-      <h1>{name}</h1>
-      <p>
+    <div className="text-center">
+      <h1 className="font-bold text-2xl">{name}</h1>
+      <p className="font-bold text-lg">
         {cuisines.join(", ")} - {costForTwoMessage}
       </p>
-      <h2>Menu</h2>
-      <ul>
-        {itemCards.map((item) => (
-          <li key={item.card.info.id}>
-            {item.card.info.name} - {"Rs. "}{" "}
-            {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
-          </li>
-        ))}
-      </ul>
+      {categories.map((category) => (
+        <RestaurantCategory data={category.card.card} />
+      ))}
     </div>
   );
 };
